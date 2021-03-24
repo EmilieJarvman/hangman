252b39d35aa4129a2a34ba716f6f2a12542fc2a7
Emilie <emilie@jarvman.com>
2021-03-24 12:47:46 +0100

    display words function

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 const correctLetters = [];
 const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord
+        .split("")
+        .map(letter => `
+        <span class="letter">
+          ${correctLetters.includes(letter) ? letter : ""}
+        </span>
+        `).join("")}
+    `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g, "");
+    
+    if (innerWord === selectedWord) {
+        finalMessage.innerText = "Congratulations! You won!";
+        popup.style.display = "flex";
+    }
+}
+
+displayWord();
